d459c166abbb879ad4be22beebbb866fa1eb3b0f
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2023-11-13 10:00:50 +0100

    Basic window with logo

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
+from tkinter import *
+from tkinter import ttk
 from coin import Wallet
 
 
 wallet = Wallet()
 
-menu_options = "\nCommands:\n\
-    - update: get latest exchange rates and your portfolio\n\
-    - buy amount coin: buy crypto, example: buy 0.1 bitcoin\n\
-    - sell amount coin: buy crypto, example: sell 9001 dogecoin\n\
-    - quit: save any changes left and quit\n"
+print(wallet)
+print(f"All your coins worth {wallet.sum_amount():.2f} USD plus {wallet.fiat} USD.")
 
-while True:
-    print(wallet)
-    print(f"All your coins worth {wallet.sum_amount():.2f} USD plus {wallet.fiat} USD.")
-    print(menu_options)
-    user_input = input("What do you want? ")
-    parts = user_input.split(" ")
-    command = parts[0]
-    if command == "update":
-        pass
-    elif command == "buy":
-        amount = float(parts[1])
-        coin = parts[2]
-        wallet.buy(amount, coin)
-    elif command == "sell":
-        amount = float(parts[1])
-        coin = parts[2]
-        wallet.sell(amount, coin)
-    elif command == "quit":
-        wallet.save()
-        break
-    else:
-        print("Unsupported command!")
+root = Tk()
+icon = PhotoImage(file="logo.png")
+root.iconphoto(True, icon)
+root.title("Stonks - trading simulator")
 
-
+root.mainloop()
