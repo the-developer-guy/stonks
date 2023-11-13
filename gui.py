48be20e0ef8b45b599164501c893e39b8bde53c0
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2023-11-13 10:51:14 +0100

    Moved Wallet to MainWindow

--- gui.py
+++ gui.py
@@ -1,11 +1,15 @@
 from tkinter import ttk
+from coin import Wallet
+
 
 class MainWindow:
     def __init__(self, root) -> None:
         self.root = root
+        self.wallet = Wallet()
+
         self.mainframe = ttk.Frame(self.root)
         self.label = ttk.Label(self.mainframe, text="Your balance:")
-        self.balance_label = ttk.Label(self.mainframe, text="x")
+        self.balance_label = ttk.Label(self.mainframe, text=self.wallet)
         self._place()
 
     def _place(self):
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from tkinter import Tk, PhotoImage
 from gui import MainWindow
-from coin import Wallet
 
-
-wallet = Wallet()
-
-print(wallet)
-print(f"All your coins worth {wallet.sum_amount():.2f} USD plus {wallet.fiat} USD.")
 
 root = Tk()
 icon = PhotoImage(file="logo.png")
